754af091f8e5cc1e20ffc87efecc30ec78a182be
jwagdav <jwagdav@gmail.com>
2022-04-17 21:41:33 -0700

    Added functionality to choose from a few different major currencies. Added a description of the crypto to the dom.

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ document.querySelector('button').addEventListener('click', getFetch);
 
 function getFetch(){
     const coin = document.querySelector('input').value;
+    const currency = document.querySelector('#currency').value;
     const url = `https://api.coingecko.com/api/v3/coins/${coin}`;
 
     fetch(url)
@@ -11,7 +12,10 @@ function getFetch(){
         .then(data => {
             console.log(data);
             const h2 = document.querySelector('h2');
-            h2.innerText += data.market_data.current_price.usd;
+            h2.innerText = `$ ${data.market_data.current_price[currency]}`;
+
+            const info = document.querySelector('.info');
+            info.innerHTML = `<h1>${data.name}<h1> </p>${data.description.en}</p>`;
         })
 
         .catch(err => {
